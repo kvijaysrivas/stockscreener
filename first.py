60bfbef45e8d0cfe391bc4468117352f29ce8cdf
vijay srivastava <kvijaysrivas@gmail.com>
2025-06-27 16:37:10 +0530

    This is second version

--- first.py
+++ first.py
@@ -1,5 +1,13 @@
+
+import sys
+import os
+
 def test_hello():
     print("Hello, World!")
+    print("Python version:", sys.version)
+    print("Current working directory:", os.getcwd())
+    print("Files in this directory:", os.listdir())
+
 
 if __name__ == "__main__":
     test_hello()
